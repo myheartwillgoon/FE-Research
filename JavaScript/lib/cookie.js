e0e7d729d73a1a338193717183d93858ac6ea598
Travel <myheartwillgoon@users.noreply.github.com>
2016-01-15 12:09:36 +0800

    Create cookie.js

---
+++ JavaScript/lib/cookie.js
@@ -0,0 +1,45 @@
+// base on https://github.com/carhartl/jquery-cookie/blob/master/src/jquery.cookie.js
+// need jQuery $.extend
+$.cookie  = function(key, value, options) {
+    var cookie, cookies, len, name, result = false
+
+    if (arguments.length > 1) {
+        options = $.extend({},options)
+
+        if (typeof options.expires === 'number') {
+            var hours = options.expires,
+                t     = options.expires = new Date()
+
+            t.setMilliseconds(t.getMilliseconds()+1000*60*60*hours)
+        }
+
+            return (document.cookie = [
+                    key,'=',value,
+                    options.expires ? '; expires=' + options.expires.toUTCString() : '',
+                    options.path ? '; path=' + options.path : '',
+                    options.domain ? '; domain=' + options.domain : '',
+                    options.secure ? '; secure' : ''
+                   ].join(''))
+
+    } else {
+        if (!key) return false
+        cookies = document.cookie ? document.cookie.split('; ') : []
+        len = cookies.length
+
+        for (var i = 0; i < len; i++) {
+            cookie = cookies[i].split('=');
+            name = cookie.shift()
+
+            if (name === key) {
+                result = cookie.join('')
+                break;
+            }
+        }
+        return result
+    }
+}
+
+$.removeCookie = function(key, options) {
+    $.cookie(key, '', $.extend({},options,{expires:-1}))
+    return !$.cookie(key)
+}
