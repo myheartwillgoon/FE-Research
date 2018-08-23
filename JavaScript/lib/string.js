b62da4d93cd2ee2a1abf4591331a5a8f3b51537c
Travel <myheartwillgoon@users.noreply.github.com>
2018-08-23 17:40:45 +0800

    Update string.js
    增加前置0消除

--- JavaScript/lib/string.js
+++ JavaScript/lib/string.js
@@ -2,5 +2,6 @@ export function onlyInputPrice(price) {
     return price.replace(/[^\d\.]/g, '')
                 .replace(/(\.(?:\d{1,2})?)[^\d]/g,'$1')
                 .replace(/^(\d+\.\d{2})([\s\S])+$/g, '$1')
-                .replace(/^(\.\d)$/g, '0$1')
+                .replace(/^(\.\d{1,2})$/g, '0$1')
+                .replace(/^0([1-9][\.\d]*)$/, '$1')
 }
