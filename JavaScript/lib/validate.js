cb249526e81809854643238955765535d08d5b05
GTravel <myheartwillgoon@users.noreply.github.com>
2018-10-31 10:35:45 +0800

    Create validate.js

---
+++ JavaScript/lib/validate.js
@@ -0,0 +1,112 @@
+/* eslint-disable */
+export const MESSAGE = {
+    emptyMobile: '请输入手机号',
+    invalidMobile: '手机号格式不正确',
+    emptySMSCode: '请输入短信验证码',
+    invalidSMSCode: '短信验证码格式错误',
+    illegalString: '不能输入特殊字符',
+    notEmpty: '不能为空',
+    outRange: '不在范围内',
+}
+
+const M = MESSAGE;
+
+const validate = {
+    isMobile(val, message = M.invalidMobile, need = true, msg = M.emptyMobile) {
+        let ret = '';
+        if (need && this.isEmpty(val)) {
+            ret = msg;
+            return ret;
+        }
+        if (!/^1\d{10}$/.test(val)) ret = message;
+        return ret;
+    },
+    isSMSCode(code, message = M.invalidSMSCode, need = true, msg = M.emptySMSCode) {
+        let ret = '';
+        if (need && this.isEmpty(code)) {
+            ret = msg;
+            return ret;
+        }
+        if (!/^\d{6}$/.test(code)) ret = message;
+        return ret;
+    },
+    isEmpty(string, message = M.notEmpty) {
+        let ret = '';
+        if (!string || (string&&!string.trim())) ret = message;
+        return ret;
+    },
+    hasEmoji(val, message = M.illegalString) {
+        let ret = '';
+        if (/(\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f])|(\ud83d[\ude80-\udeff])/.test(val)) {
+            ret = message;
+        }
+        return ret;
+    },
+    inRange(val, max, min = 0, num = false, msg = M.outRange) {
+        let len = num ? val : val.length;
+        let ret = '';
+        if (min > len || len > max) ret = msg;
+        return ret; 
+    },
+    isName(val, msg) {
+        return /^[a-zA-Z\.\u4e00-\u9fa5]+$/.test(val) ? '' : msg;
+    },
+    isNum(val, msg) {
+        return /^\d+$/.test(val) ? '' : msg;
+    },
+    isTaxNum(val, msg) {
+        return /^[0-9A-HJ-NPQRTUWXY]+$/.test(val) ? '' : msg;
+    },
+    inRules(val, ...rules) {
+        let message = '';
+        if (Array.isArray(rules[0])) rules = rules[0];
+        rules.every(item => {
+            let ret = true;
+            if (item.indexOf(':') > 0) {
+                let [rule, args] = item.split(':');
+                args = args.split('|').map(val => {
+                    if (val === 'false' || val === 'true') return val === 'false' ? false : true;
+                    else if (!isNaN(val)) return Number(val);
+                    else return val;
+                })
+                message = this[rule](val, ...args);
+            } else {
+                message = this[item](val)
+            }
+            if (message) ret = false;
+            return ret;
+        })
+        return message;
+    },
+}
+
+export class Validator {
+    constructor() {
+        this.list = [];
+    }
+    add(val, rules) {
+        if (Array.isArray(val) && Array.isArray(val[0])) {
+            this.list.concat(val);
+        } else {
+            this.list.push([val, rules]);
+        }
+    }
+    validate(allMessage = false, vm = null, keys = null) {
+        let ret;
+        this.list.every(item => {
+            const [val, rules] = item;
+            let message = '';
+            message = typeof rules === 'string' && rules.indexOf(':') === -1 ? validate[rules](val) : validate.inRules(val, rules);
+            ret = allMessage ? (ret || []).push(message) : message;
+            return allMessage ? true : !message;
+        })
+        if (allMessage && vm && keys) {
+            keys.forEach((item, i) => {
+                vm[item].message = ret[i];
+            })
+        }
+        if (allMessage && !ret.every(item => item)) ret = false;
+        return ret;
+    }
+}
+export default validate;
